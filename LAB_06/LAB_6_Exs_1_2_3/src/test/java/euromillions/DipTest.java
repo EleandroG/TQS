a79adf16b9c8e76678ff67518e151817f034d46a
Eleandro Laureano <47557405+EleandroG@users.noreply.github.com>
2021-05-03 18:31:33 +0100

    Update LAB 6_2 (Not Complete)

---
+++ LAB_06/LAB_6_Exs_1_2_3/src/test/java/euromillions/DipTest.java
@@ -0,0 +1,69 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package euromillions;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author ico0
+ */
+public class DipTest {
+
+    private Dip instance;
+
+
+    public DipTest() {
+    }
+
+    @BeforeEach
+    public void setUp() {
+        instance = new Dip(new int[]{10, 20, 30, 40, 50}, new int[]{1, 2});
+    }
+
+    @AfterEach
+    public void tearDown() {
+        instance = null;
+    }
+
+
+    @Test
+    public void testConstructorFromBadArrays() {
+        int tries_numbers = 50;
+        int tries_stars = 10;
+        int correct_numbers = 5;
+        int correct_stars = 2;
+
+        List<Integer> numbers = new ArrayList<>(Collections.nCopies(tries_numbers, 0));
+        List<Integer> stars = new ArrayList<>(Collections.nCopies(tries_stars, 0));
+
+        for(int n = 0; n < tries_numbers; n++) {
+            for(int s = 0; s < tries_stars; s++) {
+                if(n != correct_numbers && s != correct_stars) {
+                    int finalN = n;
+                    int finalS = s;
+                    assertThrows(IllegalArgumentException.class, () -> new Dip(ListToInt(numbers.subList(0, finalN)), ListToInt(stars.subList(0, finalS))));
+                }
+            }
+        }
+    }
+
+    @Test
+    public void testFormat() {
+        // note: correct the implementation of the format(), not the test...
+        String result = instance.format();
+        assertEquals("N[ 10 20 30 40 50] S[  1  2]", result, "format as string: formatted string not as expected. ");
+    }
+
+    private int[] ListToInt(List<Integer> x) {
+        return x.stream().mapToInt(i -> i).toArray();
+    }
+
+}
